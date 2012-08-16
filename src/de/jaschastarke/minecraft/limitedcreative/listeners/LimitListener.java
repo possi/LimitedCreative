298d39841f9c51f10ebf33529d344f8ae07613c9
Jascha Starke <jascha.git@ja-s.de>
2012-08-16 10:24:50 +0200

    v1.3.0b:  - Bukkit EnderChest InventoryHolder Workaround

--- src/de/jaschastarke/minecraft/limitedcreative/listeners/LimitListener.java
+++ src/de/jaschastarke/minecraft/limitedcreative/listeners/LimitListener.java
@@ -90,7 +90,7 @@ public class LimitListener implements Listener {
         
         Block block = event.getClickedBlock();
         
-        if (block.getState() instanceof InventoryHolder) {
+        if (block.getState() instanceof InventoryHolder || block.getType() == Material.ENDER_CHEST) { // Workaround, Bukkit not recognize a Enderchest
             player.onChestAccess(event);
         } else if (block.getState() instanceof Sign) {
             player.onSignAccess(event);
