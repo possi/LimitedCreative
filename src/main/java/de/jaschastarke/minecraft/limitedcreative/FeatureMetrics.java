86eeceb450833c1db9b80c623757caac9fdd686f
Jascha Starke <jascha.git@ja-s.de>
2013-07-28 11:07:27 +0200

    Added MCStats Metrics. Can be disabled together with Piwik, or like descriped at http://mcstats.org/learn-more/

--- src/main/java/de/jaschastarke/minecraft/limitedcreative/FeatureMetrics.java
+++ src/main/java/de/jaschastarke/minecraft/limitedcreative/FeatureMetrics.java
@@ -6,18 +6,46 @@ import org.bukkit.event.Listener;
 
 import de.jaschastarke.bukkit.lib.CoreModule;
 import de.jaschastarke.bukkit.tools.stats.IStatistics;
+import de.jaschastarke.bukkit.tools.stats.MCStatsMetrics;
+import de.jaschastarke.bukkit.tools.stats.MCStatsMetrics.Graph;
 import de.jaschastarke.bukkit.tools.stats.PiwikStatistics;
+import de.jaschastarke.modularize.IModule;
+import de.jaschastarke.modularize.ModuleEntry;
+import de.jaschastarke.modularize.ModuleEntry.ModuleState;
 
 public class FeatureMetrics extends CoreModule<LimitedCreative> implements Listener {
     public FeatureMetrics(LimitedCreative plugin) {
         super(plugin);
     }
     private IStatistics metric;
+    private MCStatsMetrics mcstats = null;
     
     @Override
     public void onEnable() {
         super.onEnable();
         metric = new PiwikStatistics(plugin);
+        if (mcstats == null) {
+            try {
+                mcstats = new MCStatsMetrics(plugin);
+                
+                Graph moduleGraph = mcstats.createGraph("Module Usage");
+                for (final ModuleEntry<IModule> mod : plugin.getModules()) {
+                    if (mod.getModule() instanceof CoreModule<?>) {
+                        moduleGraph.addPlotter(new MCStatsMetrics.Plotter(((CoreModule<?>) mod.getModule()).getName()) {
+                            @Override
+                            public int getValue() {
+                                return mod.getState() == ModuleState.ENABLED ? 1 : 0;
+                            }
+                        });
+                    }
+                }
+                
+                mcstats.start();
+            } catch (IOException e) {
+                // Failed to submit the stats :-(
+                getLog().warn("MCStats-Error: " + e.getMessage());
+            }
+        }
     }
     
     @Override
